d503b602c221e5de87e76895221f07452423947e
DrewWalt <djw2017@gmail.com>
2022-08-26 13:23:17 -0400

    added home and experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import SchoolIcon from "@material-ui/icons/School"
+import WorkIcon from '@material-ui/icons/Work'
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+      <VerticalTimeline lineColor='#3e497a'>
+        <VerticalTimelineElement className="vertical-timeline-element--education" date="August 2018" iconStyle={{background: '#3e497a', color: '#fff'}} icon={<SchoolIcon />}>
+          <h3 className='vertical-timeline-element-title'>Gettysburg College, Gettysburg, Pennsylvania</h3>
+          <p>Began attending Gettysburg College.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className="vertical-timeline-element--education" date="June 2019 - August 2019" iconStyle={{background: '#3e497a', color: '#fff'}} icon={<WorkIcon />}>
+          <h3 className='vertical-timeline-element-title'>Summer Intern (Technology Support) at Asturian Group, Inc.</h3>
+          <p>
+            Responsible for compilation of bid packages and electronic distribution
+            to subcontractors and vendors for pricing for federal/military
+            construction projects using Procore. Itemized take-offs using Bluebeam
+            Software. Also worked with Visual Basic and Excel to automate the
+            filling out of forms.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className="vertical-timeline-element--education" date="June 2020 - August 2020" iconStyle={{background: '#3e497a', color: '#fff'}} icon={<WorkIcon />}>
+          <h3 className='vertical-timeline-element-title'>Summer Intern (Technology Support) at Asturian Group, Inc.</h3>
+          <p>
+            Supported creation of vendor database for integration into Procore
+            (Relational Database), a construction project management and
+            estimating software. Wrote macros for database import into Procore
+            contact management module.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className="vertical-timeline-element--education" date="August 2021 - November" iconStyle={{background: '#3e497a', color: '#fff'}} icon={<WorkIcon />}>
+          <h3 className='vertical-timeline-element-title'>Software Development Engineer (Student Position) at Affordable</h3>
+          <p>
+            Updated a Selenium web browser plugin to support new inputs from
+            the user. Used AWS to setup an orchestration server. This included
+            creating an API and using API Gateway to trigger Lambda functions
+            which would then pull the requested data from the database. The
+            requested data (user info and selenium script) would then be used to
+            automatically fill out healthcare applications on websites.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className="vertical-timeline-element--education" date="May 2022" iconStyle={{background: '#3e497a', color: '#fff'}} icon={<SchoolIcon />}>
+          <h3 className='vertical-timeline-element-title'>Gettysburg College, Gettysburg, Pennsylvania</h3>
+          <p>Graduated from Gettysburg College with a Bachelor of Science degree in Computer Science.  While attending Gettysburg College I was also on the football team and a member of the Alpha Tau Omega Fraternity.</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,57 @@
 import React from 'react'
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import { IconButton } from '@material-ui/core'
+import { Email, GitHub} from '@material-ui/icons'
+import "../styles/Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="about"> 
+        <h2> Hello, My Name is Drew Walton</h2>
+        <div className="prompt"> 
+          <p>
+            I am a recent computer science graduate seeking an opportunity with an established and nationally recognized software
+            engineering firm. I want to join a team that is focused on developing innovative programming, provides
+            leadership opportunities, and invests in their employees for long-term growth and development. 
+          </p>
+            <IconButton href="https://www.linkedin.com/in/drew-walton-528029240/" target="_blank" rel="noopener noreferrer" >
+                <LinkedInIcon style={{ fill: '#0077b5' }}/>
+            </IconButton>
+
+            <IconButton href="https://github.com/DrewWalt" target="_blank" rel="noopener noreferrer">
+                <GitHub style={{ fill: 'black' }}/>
+            </IconButton>
+
+            <IconButton href="mailto:drew.walton.website@gmail.com" target="_blank" rel="noopener noreferrer">
+                <Email style={{ fill: 'deepskyblue' }}/>
+            </IconButton>
+        </div>
+      </div>
+      <div className="skills"> 
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>
+              ReactJS, CSS, HTML
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              Java Spring, MySQL, AWS lambda, MongoDB
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>
+              Java, Python, JavaScript, Assembly
+            </span>
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
